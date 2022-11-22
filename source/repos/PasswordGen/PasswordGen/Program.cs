adf3bc47ee3aff34fbcd1a969ecb72923867b39b
troskan <str4ndb3rg@live.se>
2022-11-22 09:46:06 +0100

    Cleaned up

--- source/repos/PasswordGen/PasswordGen/Program.cs
+++ source/repos/PasswordGen/PasswordGen/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 
 namespace Password_Generator
@@ -7,13 +8,14 @@ namespace Password_Generator
     {
         static void Main(string[] args)
         {
-            //Key variables
+            //Key variables.
             string passwordHint = "";
             string password = "";
             string passGenerate = "";
 
-            //Input
             Console.WriteLine("Enter a hint for the password, Facebook, Reddit, Bank");
+
+            //Input.
             passwordHint = Console.ReadLine();
 
             Console.Clear();
@@ -21,37 +23,56 @@ namespace Password_Generator
 
             StringBuilder s = new StringBuilder();
 
-
+            //Characters in the password.
             char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789#%&*".ToCharArray();
-
+            
             Random r = new Random();
 
+            //Add characters to password.
             for (int i = 0; i < r.Next(20, 30); i++)
             {
-                var charResult = alphabet[r.Next(0, alphabet.Length)];
+                //Choose a random char.
+                var addChar = alphabet[r.Next(0, alphabet.Length)];
+                int randNum = r.Next(0, 6);
 
-                Random rs = new Random();
-                int rst = rs.Next(0, 6);
-
-
-                if (rst == 3)
+                if (randNum == 3)
                 {
-                    charResult = Char.ToUpper(charResult);
-                    s.Append(charResult);
+                    addChar = Char.ToUpper(addChar);
+                    s.Append(addChar);
 
                 }
                 else
                 {
-                    charResult = Char.ToLower(charResult);
-                    s.Append(charResult);
+                    addChar = Char.ToLower(addChar);
+                    s.Append(addChar);
                 }
 
             }
-
             passGenerate = s.ToString();
             password = passwordHint + ": " + passGenerate;
-
+            
             Console.WriteLine(password);
+
+            try
+            {
+                FileStream fs = new FileStream(@"C:\Users\Alvin\Desktop\p4ssw4rds.txt", FileMode.Append);
+                StreamWriter writer = new StreamWriter(fs);
+
+                writer.WriteLine($"{password}");
+                writer.WriteLine($"");
+                writer.WriteLine($"Password created: {DateTime.Now}");
+                writer.WriteLine("---------");
+
+                writer.Close();
+                fs.Close();
+
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
             Console.ReadKey();
 
 
